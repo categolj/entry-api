bf483e7f828bc18aab9986da17868729b565b0e2
Toshiaki Maki <makingx@gmail.com>
2025-09-24 11:56:05 +0900

    Add Redis CacheErrorHandler

--- src/main/java/am/ik/blog/config/RedisConfig.java
+++ src/main/java/am/ik/blog/config/RedisConfig.java
@@ -1,12 +1,20 @@
 package am.ik.blog.config;
 
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.LoggingCacheErrorHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("redis")
 @Configuration(proxyBeanMethods = false)
 @EnableCaching
-class RedisConfig {
+class RedisConfig implements CachingConfigurer {
+
+	@Override
+	public CacheErrorHandler errorHandler() {
+		return new LoggingCacheErrorHandler(true);
+	}
 
 }
