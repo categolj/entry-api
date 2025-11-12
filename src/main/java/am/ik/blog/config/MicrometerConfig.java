0f811d5537e273427471bf00e20729b36ded56eb
Toshiaki Maki <makingx@gmail.com>
2025-11-12 20:22:40 +0900

    Disable otel semconv

--- src/main/java/am/ik/blog/config/MicrometerConfig.java
+++ src/main/java/am/ik/blog/config/MicrometerConfig.java
@@ -1,14 +1,5 @@
 package am.ik.blog.config;
 
-import io.micrometer.core.instrument.Tags;
-import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmClassLoadingMeterConventions;
-import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmCpuMeterConventions;
-import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmMemoryMeterConventions;
-import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmThreadMeterConventions;
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.core.instrument.config.MeterFilter;
 import java.util.function.Predicate;
 import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
@@ -28,27 +19,30 @@ class MicrometerConfig {
 			}));
 	}
 
-	@Bean
-	ProcessorMetrics processorMetrics() {
-		Tags extraTags = Tags.empty();
-		return new ProcessorMetrics(extraTags, new OpenTelemetryJvmCpuMeterConventions(extraTags));
-	}
-
-	@Bean
-	JvmMemoryMetrics jvmMemoryMetrics() {
-		Tags extraTags = Tags.empty();
-		return new JvmMemoryMetrics(extraTags, new OpenTelemetryJvmMemoryMeterConventions(extraTags));
-	}
-
-	@Bean
-	JvmThreadMetrics jvmThreadMetrics() {
-		Tags extraTags = Tags.empty();
-		return new JvmThreadMetrics(extraTags, new OpenTelemetryJvmThreadMeterConventions(extraTags));
-	}
-
-	@Bean
-	ClassLoaderMetrics classLoaderMetrics() {
-		return new ClassLoaderMetrics(new OpenTelemetryJvmClassLoadingMeterConventions());
-	}
+	// @Bean
+	// ProcessorMetrics processorMetrics() {
+	// Tags extraTags = Tags.empty();
+	// return new ProcessorMetrics(extraTags, new
+	// OpenTelemetryJvmCpuMeterConventions(extraTags));
+	// }
+	//
+	// @Bean
+	// JvmMemoryMetrics jvmMemoryMetrics() {
+	// Tags extraTags = Tags.empty();
+	// return new JvmMemoryMetrics(extraTags, new
+	// OpenTelemetryJvmMemoryMeterConventions(extraTags));
+	// }
+	//
+	// @Bean
+	// JvmThreadMetrics jvmThreadMetrics() {
+	// Tags extraTags = Tags.empty();
+	// return new JvmThreadMetrics(extraTags, new
+	// OpenTelemetryJvmThreadMeterConventions(extraTags));
+	// }
+	//
+	// @Bean
+	// ClassLoaderMetrics classLoaderMetrics() {
+	// return new ClassLoaderMetrics(new OpenTelemetryJvmClassLoadingMeterConventions());
+	// }
 
 }
